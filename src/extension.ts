eb6d54738143c45528a4363719ca73e29652d049
Lin Zhang <linroid@gmail.com>
2019-12-10 23:04:58 +0800

    Remove useless logs

--- src/extension.ts
+++ src/extension.ts
@@ -14,8 +14,6 @@ const watchers = new Map<string, vscode.FileSystemWatcher>();
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "dora-vscode" is now active!');
-  console.log(vscode.workspace.getConfiguration('dorajs'));
   bindWatcher();
   vscode.window.onDidChangeActiveTextEditor(bindWatcher);
   context.subscriptions.push(
